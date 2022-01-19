fff42e3d6092bbf0c8c9471096cba7757a479a1d
Seamus McGill <seamusmcgill@gmail.com>
2022-01-19 15:59:54 -0500

    Updated input.js to incorporate game controls

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
 const net = require("net");
 
-const setupInput = function () {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,13 +12,25 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = (data) => {
-  if (data === "SIGINT") {
+const handleUserInput = (key) => {
+  // If key pressed is ctrl+c
+  if (key === "\u0003") {
     process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
   }
 };
 
 module.exports = {
   setupInput,
 };
- 
--- play.js
+++ play.js
@@ -1,7 +1,8 @@
-const net = require("net");
 const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
-setupInput();
+
+let conn = connect();
+
+setupInput(conn);
