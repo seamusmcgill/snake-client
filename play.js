c30302539f25a875f15ac47e086ad53ead48016e
Seamus McGill <seamusmcgill@gmail.com>
2022-01-19 14:41:37 -0500

    Added setupInput and handleUserInput functions, allow user to exit game

--- play.js
+++ play.js
@@ -1,5 +1,20 @@
 const net = require("net");
 const { connect } = require("./client");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (data) => {
+  if (data === "SIGINT") {
+    process.exit();
+  }
+};
+ 
 console.log("Connecting ...");
 connect();
